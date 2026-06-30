2bfb12f83a333f7e3f74064cc99c8aacedef8471
Muftawu <mohammedyiwere@gmail.com>
2026-06-30 06:52:46 +0000

    added floating windows for lsp errors

--- init.lua
+++ init.lua
@@ -383,8 +383,13 @@ require('lazy').setup({
       -- See :help vim.diagnostic.Opts
       vim.diagnostic.config {
         severity_sort = true,
-        float = { border = 'rounded', source = 'if_many' },
-        underline = { severity = vim.diagnostic.severity.ERROR },
+        float = {
+          border = 'rounded',
+          source = 'if_many',
+        },
+        underline = {
+          severity = vim.diagnostic.severity.ERROR,
+        },
         signs = vim.g.have_nerd_font and {
           text = {
             [vim.diagnostic.severity.ERROR] = '󰅚 ',
@@ -393,19 +398,20 @@ require('lazy').setup({
             [vim.diagnostic.severity.HINT] = '󰌶 ',
           },
         } or {},
-        virtual_text = {
-          source = 'if_many',
-          spacing = 2,
-          format = function(diagnostic)
-            local diagnostic_message = {
-              [vim.diagnostic.severity.ERROR] = diagnostic.message,
-              [vim.diagnostic.severity.WARN] = diagnostic.message,
-              [vim.diagnostic.severity.INFO] = diagnostic.message,
-              [vim.diagnostic.severity.HINT] = diagnostic.message,
-            }
-            return diagnostic_message[diagnostic.severity]
-          end,
-        },
+        virtual_text = false,
+        -- virtual_text = {
+        --   source = 'if_many',
+        --   spacing = 2,
+        --   format = function(diagnostic)
+        --     local diagnostic_message = {
+        --       [vim.diagnostic.severity.ERROR] = diagnostic.message,
+        --       [vim.diagnostic.severity.WARN] = diagnostic.message,
+        --       [vim.diagnostic.severity.INFO] = diagnostic.message,
+        --       [vim.diagnostic.severity.HINT] = diagnostic.message,
+        --     }
+        --     return diagnostic_message[diagnostic.severity]
+        --   end,
+        -- },
       }
 
       local capabilities = require('blink.cmp').get_lsp_capabilities()
@@ -634,5 +640,16 @@ require('lazy').setup({
   },
 })
 
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+-- custom config
+vim.keymap.set('n', '<leader>n', ':Neotree toggle<CR>')
+vim.api.nvim_create_user_command('Luatoogood', function()
+  vim.fn.system { 'notify-send', 'HELLO', 'LOVE NVIM SCRIPTING WITH LUA' }
+end, {})
+
+vim.api.nvim_create_user_command('Nfp', function(opts)
+  vim.fn.system { 'touch', './' .. opts.args }
+  print('🟢 File created [' .. opts.args .. ']')
+end, { nargs = '*' })
+
+-- diagnostic tool for lsp servers
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
